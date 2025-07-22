79a7bbea1fcd45bfc1790334eae99a7a3c05544d
Hossam Mostafa <hossamarab2024@gmail.com>
2025-07-22 10:50:23 +0300

    handle startup

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { AuthNavComponent } from '../../components/auth-nav/auth-nav.component';
+import { AuthFooterComponent } from '../../components/auth-footer/auth-footer.component';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  imports: [AuthNavComponent, AuthFooterComponent,RouterOutlet],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
--- src/app/layouts/user-layout/user-layout.component.ts
+++ src/app/layouts/user-layout/user-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "../../pages/home/home.component";
+import { UserNavComponent } from "../../components/user-nav/user-nav.component";
+import { UserFooterComponent } from "../../components/user-footer/user-footer.component";
 
 @Component({
   selector: 'app-user-layout',
-  imports: [],
+  imports: [HomeComponent, UserNavComponent, UserFooterComponent],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
