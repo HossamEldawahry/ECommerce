564490c9301ba918d667c5fcbc9a03a0b393bacf
Hossam Mostafa <hossamarab2024@gmail.com>
2025-07-22 09:23:40 +0300

    Create auth-user basic folders

---
+++ src/app/components/auth-nav/auth-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-nav',
+  imports: [],
+  templateUrl: './auth-nav.component.html',
+  styleUrl: './auth-nav.component.scss'
+})
+export class AuthNavComponent {
+
+}
